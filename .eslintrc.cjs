0f1171ef831f7ab13907d31527c4cf7c97975800
dohaleex <dohaleex@gmail.com>
2023-11-23 23:31:51 +0900

    feat: 파이어베이스 프로젝트 생성 및 사용(SDK)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,7 +9,7 @@ module.exports = {
     'plugin:react-hooks/recommended',
     'prettier',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts', 'firebase.ts'],
   parser: '@typescript-eslint/parser',
   parserOptions: { project: './tsconfig.json' },
   plugins: ['react-refresh'],
---
+++ firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCIlyy70YWfENyZ8K3OyEDDM0gJcjymVv8',
+  authDomain: 'twitter-clone-xx.firebaseapp.com',
+  projectId: 'twitter-clone-xx',
+  storageBucket: 'twitter-clone-xx.appspot.com',
+  messagingSenderId: '374291609897',
+  appId: '1:374291609897:web:d143692e1881c2fca89f38',
+};
+
+const app = initializeApp(firebaseConfig);
