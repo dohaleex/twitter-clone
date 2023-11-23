7cf1363f814188c1d1ea40b53567315355da36f8
dohaleex <dohaleex@gmail.com>
2023-11-24 00:23:17 +0900

    feat: 글로벌스타일 적용 및 로딩화면 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import * as styled from 'styled-components';
+import reset from 'styled-reset';
+import { useEffect, useState } from 'react';
 import CreateAccount from './routes/create-account';
 import Login from './routes/login';
 import Profile from './routes/profile';
 import Layout from './components/layout';
 import Home from './routes/home';
+import LoadingScreen from './components/loading-screen';
 
 const router = createBrowserRouter([
   {
@@ -30,10 +34,44 @@ const router = createBrowserRouter([
   },
 ]);
 
+const GlobalStyle = styled.createGlobalStyle`
+  ${reset}
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    background-color: black;
+    color: white;
+    font-family:
+      system-ui,
+      -apple-system,
+      BlinkMacSystemFont,
+      'Segoe UI',
+      Roboto,
+      Oxygen,
+      Ubuntu,
+      Cantarell,
+      'Open Sans',
+      'Helvetica Neue',
+      sans-serif;
+  }
+`;
+
 function App() {
+  const [isLoading, setLoading] = useState(true);
+
+  const init = async () => {
+    setTimeout(() => setLoading(false), 1000);
+  };
+
+  useEffect(() => {
+    init();
+  }, []);
+
   return (
     <div>
-      <RouterProvider router={router} />
+      <GlobalStyle />
+      {isLoading ? <LoadingScreen /> : <RouterProvider router={router} />}
     </div>
   );
 }
