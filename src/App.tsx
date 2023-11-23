3af783d4e75f35dd2e8b0fc046080a9036e7ee11
dohaleex <dohaleex@gmail.com>
2023-11-23 22:32:00 +0900

    feat: 브라우저 경로 설정
    기본경로와 메인('/')에서 로그인 상태에 따라 접근경로를 다르게 하는 중첩경로(Outlet)를 추가함

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,41 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import CreateAccount from './routes/create-account';
+import Login from './routes/login';
+import Profile from './routes/profile';
+import Layout from './components/layout';
+import Home from './routes/home';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/profile',
+        element: <Profile />,
+      },
+    ],
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/create-account',
+    element: <CreateAccount />,
+  },
+]);
+
 function App() {
-  return <div />;
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
