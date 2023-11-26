cebf4ee1672ea5271b400620e91cdf485b7c5a1b
dohaleex <dohaleex@gmail.com>
2023-11-26 22:28:44 +0900

    feat: 트윗 데이터를 가져오는 이벤트 구독을 취소하기
    DB 데이터사용량이 낭비되는것을 방지하기 위해,
    이벤트리스너가 동작할 필요가 없는 경우, 해당 이벤트 구독을 취소하는 기능을 활성화

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -20,5 +20,7 @@ module.exports = {
     'no-alert': 'off',
     'react/jsx-props-no-spreading': 'off',
     'import/no-cycle': 'off',
+    'no-unused-expressions': 'off',
+    '@typescript-eslint/no-unused-expressions': 'off',
   },
 };
